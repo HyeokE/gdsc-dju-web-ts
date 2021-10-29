99256b0e373ae38b85ab5dd18f45d7b4e5c92612
HYEOK_E <jhjeong00@gmail.com>
2021-10-29 18:02:13 +0900

    feat : project setting

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseKey = {
+  apiKey: 'AIzaSyB-vR8wfhXZp-YtlSw4_QN5A5sh_5dLd5g',
+  authDomain: 'gdsc-dju-web.firebaseapp.com',
+  projectId: 'gdsc-dju-web',
+  storageBucket: 'gdsc-dju-web.appspot.com',
+  messagingSenderId: '328000675982',
+  appId: '1:328000675982:web:2fb47d16657c7b87ba69df',
+  measurementId: 'G-RDSQPRD3ME',
+};
---
+++ src/firebase/firebase.tsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import { firebaseKey as firebaseConfig } from './firebase.config';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+export const dbService = firebase.firestore();
+export const authService = firebase.auth();
