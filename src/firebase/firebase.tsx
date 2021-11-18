6d50bb62d005d47d358915675db01d392c545a79
HYEOK_E <jhjeong00@gmail.com>
2021-11-18 20:49:29 +0900

    chore : google analytics 설정

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -4,7 +4,8 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 import { firebaseKey as firebaseConfig } from './firebase.config';
-
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +13,8 @@ import { firebaseKey as firebaseConfig } from './firebase.config';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 firebase.initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const firebaseInstance = firebase;
 export const dbService = firebase.firestore();
 export const authService = firebase.auth();
