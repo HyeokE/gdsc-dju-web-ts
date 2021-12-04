ce36994025ff38e8ab9fad64146242ca3a3bf569
HYEOK_E <jhjeong00@gmail.com>
2021-12-04 18:20:03 +0900

    feat : 페이지 이동시 스크롤이 초기화되도록 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollTop from './components/common/ScrollTop';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
