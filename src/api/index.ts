cd2e984948f3d2e66fa70e51e36ec790a3b98b78
HYEOK_E <jhjeong00@gmail.com>
2021-12-31 03:28:51 +0900

    backend api connect

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,14 +9,15 @@ export class GDSCApi {
   constructor() {
     this.ANALYTICS_API = 'https://www.googleapis.com/auth/analytics';
     this.API =
-      'https://firestore.googleapis.com/v1/projects/gdsc-dju/databases/(default)';
+      'http://ec2-15-165-170-84.ap-northeast-2.compute.amazonaws.com:8080';
   }
   getMembers = () => {
     return axios.get<UserDataState>(`${this.API}/documents/members`);
   };
-  postOnboardingMembers = () => {
-    return axios.get<typeof OnboardingUserState>(
-      `${this.API}/documents/members`,
+  postOnboardingMembers = (payload: typeof OnboardingUserState) => {
+    return axios.post<typeof OnboardingUserState>(
+      `${this.API}/api/onBoading/nickname`,
+      payload,
     );
   };
 }
