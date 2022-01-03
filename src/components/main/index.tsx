0f17f38eeb8e98398e6d839072485e394215ef48
HYEOK_E <jhjeong00@gmail.com>
2022-01-03 20:13:29 +0900

    chore: import 변경

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -7,11 +7,7 @@ import { alertState } from '../../store/alert';
 import Navigation from '../common/Navigation';
 import { Footer } from '../common/Footer';
 import MobileMenu from '../common/Navigation/MobileMenu';
-import MobileMenuCategory from '../common/Navigation/MobileMenuCategory';
-import { MobileNavBackGround } from '../common/Navigation/MobileMenu/styled';
-import { sidebar } from '../common/Variants/NavigationAnimation';
 import { menuState } from '../../store/menu';
-import { ShortNavigation } from '../common/Navigation/styled';
 
 export const Main = () => {
   const [alert] = useRecoilState(alertState);
