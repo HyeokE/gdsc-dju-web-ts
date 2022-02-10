b5fbdc43740c37bd5091c20546c5b2a3c5b0616b
HYEOK_E <jhjeong00@gmail.com>
2022-02-10 14:33:04 +0900

    feate: OAuth api add

--- src/api/index.ts
+++ src/api/index.ts
@@ -11,8 +11,11 @@ import { OnboardingUserState } from '../store/onboardingUser';
 
 export class GDSCApi {
   private API: string;
+  private OauthAPI: string;
   constructor() {
     this.API = 'https://gdsc-dju.com';
+    this.OauthAPI =
+      'https://www.googleapis.com/oauth2/v2/userinfo?access_token=';
   }
   getMemberNickname = () => {
     return axios.get<nickNameDataType>(
@@ -35,6 +38,14 @@ export class GDSCApi {
       `${this.API}/api/support/limit/update`,
       payload,
     );
+  };
+  getOauthUser = (token: string) => {
+    return axios.get(`${this.OauthAPI}${token}`, {
+      headers: {
+        authorization: `token ${token}`,
+        accept: 'application/json',
+      },
+    });
   };
 }
 export default new GDSCApi();
